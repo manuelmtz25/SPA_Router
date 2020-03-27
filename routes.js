7d6f4c1c083e1cf137a6880feef43b8992a7bba8
manuelmtz25 <manuelmtz_25@hotmail.com>
2020-03-27 15:55:48 -0600

    Estructura inicial

---
+++ routes.js
@@ -0,0 +1,14 @@
+const routes = [
+    {
+        path:'/',
+        template: '<h1>Hola</h1>'
+    },
+    {
+        path:'/contact',
+        template: '<h1>Contacto</h1>'
+    },
+    {
+        path:'/about',
+        template: '<h1>About me</h1>'
+    },
+]
